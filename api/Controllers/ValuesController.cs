62e4c3bf7f414c04509e579a10c23d4615b81b3b
Zhang Leyu <LY2125@mizuhobank.com.sg>
2018-09-11 15:34:36 +0800

    protecting api/values also.

--- api/Controllers/ValuesController.cs
+++ api/Controllers/ValuesController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
 namespace api.Controllers {
     [Route ("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ValuesController : ControllerBase {
         // GET api/values
         [HttpGet]
