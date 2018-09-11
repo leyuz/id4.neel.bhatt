a536d799204ecf78734120763a996de3d34a119f
Zhang Leyu <LY2125@mizuhobank.com.sg>
2018-09-11 15:31:58 +0800

    all running

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,31 +1,45 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
 
 namespace Client {
-    public class Program {
+    class Program {
         public static void Main (string[] args) => MainAsync ().GetAwaiter ().GetResult ();
 
         private static async Task MainAsync () {
-            // discover endpoints from the metadata by calling Auth server hosted on 5000 port
-            var discoveryClient = await DiscoveryClient.GetAsync ("http://localhost:5000");
-            if (discoveryClient.IsError) {
-                Console.WriteLine (discoveryClient.Error);
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync ("http://localhost:5000");
+            if (disco.IsError) {
+                Console.WriteLine (disco.Error);
                 return;
             }
 
-            // request the token from the Auth server
-            var tokenClient = new TokenClient (discoveryClient.TokenEndpoint, "client", "secret");
-            var response = await tokenClient.RequestClientCredentialsAsync ("api1");
+            // request token
+            var tokenClient = new TokenClient (disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync ("api1");
 
-            if (response.IsError) {
-                Console.WriteLine (response.Error);
+            if (tokenResponse.IsError) {
+                Console.WriteLine (tokenResponse.Error);
                 return;
             }
 
-            Console.WriteLine (response.Json);
+            Console.WriteLine (tokenResponse.Json);
+            Console.WriteLine ("\n\n");
 
+            // call api
+            var client = new HttpClient ();
+            client.SetBearerToken (tokenResponse.AccessToken);
+
+            var response = await client.GetAsync ("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode) {
+                Console.WriteLine ("Failed");
+                Console.WriteLine (response.StatusCode);
+            } else {
+                var content = await response.Content.ReadAsStringAsync ();
+                Console.WriteLine (JArray.Parse (content));
+            }
         }
-
     }
 }
--- api/Startup.cs
+++ api/Startup.cs
@@ -37,6 +37,7 @@ namespace api {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseAuthentication ();
             if (env.IsDevelopment ()) {
                 app.UseDeveloperExceptionPage ();
             } else {
@@ -46,7 +47,6 @@ namespace api {
             app.UseHttpsRedirection ();
             app.UseMvc ();
 
-            app.UseAuthentication ();
         }
     }
 }
